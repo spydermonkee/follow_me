50fd332be9a3463bab5e73b1bc62de1142d8bdb5
Spyder McHenry <spyder@notch8.com>
2014-12-03 21:18:28 -0800

    Adds twitter app client

---
+++ config/initializers/twitter_client.rb
@@ -0,0 +1,6 @@
+TWITTER_CLIENT = Twitter::REST::Client.new do |config|
+  config.consumer_key        = TWITTER_CONSUMER_KEY
+  config.consumer_secret     = TWITTER_CONSUMER_SECRET
+  config.access_token        = TWITTER_ACCESS_TOKEN
+  config.access_token_secret = TWITTER_ACCESS_TOKEN_SECRET
+end
