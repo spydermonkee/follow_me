b2b83ccece13df21501f70ffd7dd9bebf228802a
Evan Carmi <evan@ecarmi.org>
2014-12-26 11:47:50 -0800

    Create Users#posts controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,16 +62,19 @@ class UsersController < ApplicationController
   end
 
   def posts
-    render json: "hello world"
+    tweets = TWITTER_CLIENT.user_timeline(params[:username], count: 100).select(&:geo?)
+    render json: tweets
   end
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params[:user]
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params[:user]
+  end
 end
